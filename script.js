311839108fd479134a966859f4e22bae7d479048
jeffhkelly <146142676+jeffhkelly@users.noreply.github.com>
2023-11-25 23:03:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ function calcularCusto() {
         document.getElementById("resultado").innerHTML = "Custo de óleo: R$ " + custoTotal.toFixed(2);
     }
 }
+function calcularCusto() {
+    var km = parseFloat(document.getElementById("km").value);
+    var valorLitro = parseFloat(document.getElementById("valorLitro").value);
+    var consumoKmPorLitro = parseFloat(document.getElementById("consumoKmPorLitro").value);
+    var valorRegularizacaoAnual = parseFloat(document.getElementById("valorRegularizacaoAnual").value);
+    var vidaUtilPneuDianteiro = parseFloat(document.getElementById("vidaUtilPneuDianteiro").value);
+    var vidaUtilPneuTraseiro = parseFloat(document.getElementById("vidaUtilPneuTraseiro").value);
+    var gastosPessoais = parseFloat(document.getElementById("gastosPessoais").value);
+
+    // Realize os cálculos desejados aqui
+
+    var resultado = document.getElementById("resultado");
+    // Atualize o elemento "resultado" com o resultado dos cálculos
+}
